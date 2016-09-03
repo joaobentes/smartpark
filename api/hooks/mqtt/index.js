ee66e213360c4194e8615015e563a4bc601c0bad
Joao Bentes <joaobentes.junior@gmail.com>
2016-09-03 17:05:34 +0000

    Fixed MQTT server issue

--- api/hooks/mqtt/index.js
+++ api/hooks/mqtt/index.js
@@ -1,13 +1,10 @@
-var mqtt = require("mqtt");
-var client  = mqtt.connect('tcp://138.68.139.166');
-
 function updateSpotAvailability(id, status) {
-    ParkingSpot.update({id: id}, {isAvailable: status})
+    ParkingSpot.update({id: id}, {status: status})
 	.exec(function(err, spot){
 	    if(err) {
 		console.log(err)
 	    } else {
-		console.log("Availability of " + spot.id + " set as " + spot.isAvailable)
+		console.log("Availability of " + spot.id + " set as " + spot.status)
 	    };
 	});
 
@@ -22,33 +19,41 @@ module.exports = function mqtt(sails) {
 	    sails.on('hook:orm:loaded', function() {
 
 		console.log("Mqtt on");
+
+		// Set up the client
+		var mqtt = require("mqtt");
+
+		var options = {
+		    protocolId: 'MQIsdp',
+		    protocolVersion: 3
+		};
+
+		var client = mqtt.connect("mqtt://localhost", options);
 		
 		// Subscribe to the presence channel
 		client.on('connect', function () {
 		    console.log("Sub to channels");
 		    client.subscribe('spot/occupied');
 		    client.subscribe('spot/available');
-		    client.publish('spot/occupied', '123');
-		    client.publish('spot/available', '123');
 		});
 
-		console.log(client);
-
+		// Process message on the topics
 		client.on('message', function (topic, message) {
 		    console.log('Always on');
-		    console.log(topic);
-		    console.log(message.toString());
 		    if(topic === 'spot/occupied') {
-			console.log("Mqtt");
-			console.log(mesasge.toString());
-			//updateSpotAvailability(message.toString(), false);
-		    } else if (topic == 'spot/available') {
-			console.log("Mqtt");
-			console.log(mesasge);
-			//updateSpotAvailability(message.toString(), true);
+			console.log(message.toString());
+			//updateSpotAvailability(message.toString(), 'occupied');
+		    } else if (topic === 'spot/available') {
+			console.log(message);
+			//updateSpotAvailability(message.toString(), 'available');
 		    } else {
 			console.log('Unknow message');
 		    }
+		});
+
+		// Get errors
+		client.on('error', function(){
+		    console.log("ERROR");
 		    client.end();
 		});
 		
