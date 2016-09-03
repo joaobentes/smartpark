11613ae726d5ae88ebe2ca8ed2823836ea705f97
Joao Bentes <joaobentes.junior@gmail.com>
2016-09-03 11:45:10 +0000

    Set-up the requet package and custom routes for data request

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
 
   /***************************************************************************
   *                                                                          *
@@ -46,4 +46,6 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
+   // Request available parking spots
+   'GET /getParkingSpots': {controller: "ParkingSpotController", action: "getAvailableSpots"},
 };
