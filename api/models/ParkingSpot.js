b6654da9049f58b404c12dc1805d75e4bc05d63d
Joao Bentes <joaobentes.junior@gmail.com>
2016-09-03 12:19:25 +0000

    Created a model for private parking spots

--- api/models/ParkingSpot.js
+++ api/models/ParkingSpot.js
@@ -3,12 +3,31 @@
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ * @param paymentType : 0 - credit card
  */
 
 module.exports = {
 
-  attributes: {
-
-  }
+    attributes: {
+	latitude: {
+	    type: 'string'
+	},
+	longitude: {
+	    type: 'string'
+	},
+	parkingType: {
+	    type: 'string'
+	},
+	paymentType: {
+	    type: 'integer'	    
+	},
+	availableFrom: {
+	    type: 'date'
+	},
+	availableTo: {
+	    type: 'date'
+	}
+    }
+    
 };
 
